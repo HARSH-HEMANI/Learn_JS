0f5a9162de883fde2ce2c45366b8e0186d6886f1
harsh hemani <hhemani365@gmail.com>
2026-01-11 15:31:00 +0530

    Datatype_Summary

---
+++ datatypes_summary.js
@@ -0,0 +1,61 @@
+/*
+// premitive 
+
+they are generally call by value. Here, copy of the actual data is used.
+changes are done in copy
+
+7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+JavaScript is dynamically typed language, that we don't need to explicitly declare the data type of a variable  
+
+
+// Reference type / Non-premitive
+
+call by refrence.
+Array, Object, Functions
+
+*/
+
+const score = 100
+const scoreValue = 100.3 // both score and scorevalue are number
+
+const isLoggedIn = false
+const outsideTemp = null // datatype -- object 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log( id === anotherId); //false
+
+const bigNumber = 321548978945613245789465132
+
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj ={
+    name : "hitesh",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello! World...");    
+}
+
+// console.log(typeof <varibalename>); to check datatype of any variable
+
+console.log(typeof myFunction); //object
+
+
+/*
+
+type of val         Result
+
+Undefined           "undefined"
+Null                "object"
+Boolean             "boolean"
+Number              "number"
+String              "string"
+non-premitives are all objects
+*/
+
+
